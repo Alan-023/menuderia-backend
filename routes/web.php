e11bb3f3babbedfd02f34bcc597aa9789ea36f97
Alan-023 <alan.avinav@gmail.com>
2026-05-20 12:57:32 -0600

    Pro tip para correr migraciones

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/correr-migraciones', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return "Migraciones coronadas con exito en la nube!";
+});
